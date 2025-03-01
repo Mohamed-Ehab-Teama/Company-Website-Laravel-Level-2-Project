bee9105f7d687e69a6109fc00837ca4ce2cec85f
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-03-01 07:24:47 +0200

    Front Theme Integeration - extra edit

--- resources/views/components/hero-component.blade.php
+++ resources/views/components/hero-component.blade.php
@@ -3,8 +3,8 @@
         <h1 class="text-white animated zoomIn mb-3"> {{ $title }} </h1>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
-                <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                <li class="breadcrumb-item"><a class="text-white" href="{{route('front.index')}}">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-white">Pages</a></li>
                 <li class="breadcrumb-item text-white active" aria-current="page"> {{ $subtitle }} </li>
             </ol>
         </nav>
