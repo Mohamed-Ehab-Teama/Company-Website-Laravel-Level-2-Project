e2f97c86cd0fcebeef78b7fd95a253948846e269
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-03-26 13:34:37 +0200

    Arabic Tranlations

--- lang/ar/keywords.php
+++ lang/ar/keywords.php
@@ -17,4 +17,78 @@ return [
     'profile' => 'الملف الشخصي',
     'setting' => 'الإعدادات',
     'activities' => 'النشاطات',
+
+    // SideBar
+    'components'    => 'المكونات',
+    'services'      => 'الخدمات',
+    'features'      => 'مميزات',
+
+    // Common Page
+    'created_successfully'  => 'تم تكوين الخدمة بنجاح',
+    'updated_successfully'  => 'تم تعديل الخدمة بنجاح',
+    'deleted_successfully'  => 'تم حذف الخدمة بنجاح',
+    'no_data'               => 'لا توجد بيانات',
+    'add_new'               => 'إضافة جديد',
+    'action'                => 'الأحداث',
+    'go_back'               => 'للخلف',
+    'submit'               => 'إرسال',
+    'update'               => 'تحديث',
+
+    // Services Page
+    'name'              => 'الاسم',
+    'icon'              => 'الرمز',
+    'description'       => 'الوصف',
+    'service'           => 'الخدمة',
+    'title'             => 'العنوان',
+    'create_service'    => 'إضافة خدمة',
+    'show_service'      => 'تعديل الخدمة',
+    'edit_service'      => 'حذف الخدمة',
+
+    // Fratures Page
+    'feature'           => 'الميزة',
+    'create_feature'    => 'إضافة ميزة',
+    'show_feature'      => 'تعديل الميزة',
+    'edit_feature'      => 'حذف الميزة',
+
+    // Messages Page
+    'subject'           => 'الموضوع',
+    'message'           => 'الرسالة',
+    'messages'          => 'الرسائل',
+    'show_message'      => 'عرض الرسالة',
+    'created_at'        => 'وقت التكوين',
+
+    // Subscribers Page
+    'subscribers'           => 'المشاركون',
+    'subscribtion_date'     => 'تاريخ الإشتراك',
+
+    // Testimonials Page
+    'testimonials'          => 'الآراء',
+    'position'              => 'المسمي الوظيفي',
+    'image'                 => 'الصورة',
+    'create_testimonial'    => 'إضافة رأي',
+    'show_testimonial'      => 'عرض الرأي',
+    'edit_testimonial'      => 'تعديل الرأي',
+
+    // companies Page
+    'companies'             => 'الشركات',
+    'create_company'        => 'إضافة شركة',
+    'show_company'          => 'عرض الشركة',
+    'edit_company'          => 'تعديل الشركة',
+
+    // members Page
+    'members'               => 'الأعضاء',
+    'facebook'              => 'فيسبوك',
+    'twitter'               => 'تويتر',
+    'linkedin'              => 'لنكد إن',
+    'create_member'         => 'إضافة عضو',
+    'show_member'           => 'عرض العضو',
+    'edit_member'           => 'تعديل العضو',
+
+    // settings Page
+    'settings'              => 'الإعدادات',
+    'address'               => 'العنوان',
+    'phone'                 => 'رقم الهاتف',
+    'youtube'               => 'يوتيوب',
+    'instagram'             => 'إنستجرام',
+
 ];
