def126388fdc04699c8fae5941a097790135e2d3
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-04-16 21:02:59 +0200

    Finalize the Documentation and the project

--- routes/api.php
+++ routes/api.php
@@ -36,67 +36,70 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/logout', 'logout')->middleware('auth:sanctum');
 });
 
-// Services API
-Route::controller(ServiceController::class)->group(function () {
-    route::get('/services', 'index');
-    route::post('/services/create', 'store');
-    route::get('/services/show/{id}', 'show');
-    route::post('/services/update/{id}', 'update');
-    route::get('/services/delete/{id}', 'destroy');
-});
+Route::middleware('auth:sanctum')->group(function () {
 
-// Features API
-Route::controller(FeatureController::class)->group(function () {
-    Route::get('/features', 'index');
-    Route::get('/features/{id}', 'show');
-    Route::post('/features/create', 'store');
-    Route::post('/features/update/{id}', 'update');
-    Route::get('/features/delete/{id}', 'destroy');
-});
+    // Services API
+    Route::controller(ServiceController::class)->group(function () {
+        route::get('/services', 'index');
+        route::post('/services/create', 'store');
+        route::get('/services/show/{id}', 'show');
+        route::post('/services/update/{id}', 'update');
+        route::get('/services/delete/{id}', 'destroy');
+    });
 
-// Messages Module
-Route::controller(MessageController::class)->group(function () {
-    Route::get('/messages', 'index');
-    Route::get('/message/{id}', 'show');
-    Route::post('messages/create', 'store');
-    Route::get('/message/delete/{id}', 'destroy');
-});
+    // Features API
+    Route::controller(FeatureController::class)->group(function () {
+        Route::get('/features', 'index');
+        Route::get('/features/{id}', 'show');
+        Route::post('/features/create', 'store');
+        Route::post('/features/update/{id}', 'update');
+        Route::get('/features/delete/{id}', 'destroy');
+    });
 
-// Settings Module
-Route::controller(SettingController::class)->group(function () {
-    Route::get('/settings', 'index');
-    Route::post('/settings/update', 'update');
-});
+    // Messages Module
+    Route::controller(MessageController::class)->group(function () {
+        Route::get('/messages', 'index');
+        Route::get('/message/{id}', 'show');
+        Route::post('messages/create', 'store');
+        Route::get('/message/delete/{id}', 'destroy');
+    });
 
-// Subscribers Module
-Route::controller(SubscriberController::class)->group(function () {
-    Route::get('/subscribers', 'index');
-    Route::post('/subscribers/create', 'store');
-});
+    // Settings Module
+    Route::controller(SettingController::class)->group(function () {
+        Route::get('/settings', 'index');
+        Route::post('/settings/update', 'update');
+    });
 
-// Testimonials Module
-Route::controller(TestimonialController::class)->group(function () {
-    Route::get('/testimonials', 'index');
-    Route::get('/testimonials/show/{id}', 'show');
-    Route::post('/testimonials/create', 'store');
-    Route::post('/testimonials/update/{id}', 'update');
-    Route::get('/testimonials/delete/{id}', 'destroy');
-});
+    // Subscribers Module
+    Route::controller(SubscriberController::class)->group(function () {
+        Route::get('/subscribers', 'index');
+        Route::post('/subscribers/create', 'store');
+    });
 
-// Comapnies Module
-Route::controller(CompanyController::class)->group(function () {
-    Route::get('/companies', 'index');
-    Route::get('/companies/show/{id}', 'show');
-    Route::post('/companies/create', 'store');
-    Route::post('/companies/update/{id}', 'update');
-    Route::get('/companies/delete/{id}', 'destroy');
-});
+    // Testimonials Module
+    Route::controller(TestimonialController::class)->group(function () {
+        Route::get('/testimonials', 'index');
+        Route::get('/testimonials/show/{id}', 'show');
+        Route::post('/testimonials/create', 'store');
+        Route::post('/testimonials/update/{id}', 'update');
+        Route::get('/testimonials/delete/{id}', 'destroy');
+    });
 
-// Members Module
-Route::controller(MemberController::class)->group(function () {
-    Route::get('/members', 'index');
-    Route::get('/members/show/{id}', 'show');
-    Route::post('/members/create', 'store');
-    Route::post('/members/update/{id}', 'update');
-    Route::get('/members/delete/{id}', 'destroy');
+    // Comapnies Module
+    Route::controller(CompanyController::class)->group(function () {
+        Route::get('/companies', 'index');
+        Route::get('/companies/show/{id}', 'show');
+        Route::post('/companies/create', 'store');
+        Route::post('/companies/update/{id}', 'update');
+        Route::get('/companies/delete/{id}', 'destroy');
+    });
+
+    // Members Module
+    Route::controller(MemberController::class)->group(function () {
+        Route::get('/members', 'index');
+        Route::get('/members/show/{id}', 'show');
+        Route::post('/members/create', 'store');
+        Route::post('/members/update/{id}', 'update');
+        Route::get('/members/delete/{id}', 'destroy');
+    });
 });
